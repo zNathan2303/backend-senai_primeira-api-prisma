23940dee6ea3ac57f8b882e12139f57480ad46e6
NATHAN DA SILVA COSTA <25132656@CFP127ALUNO.LOCAL>
2025-10-01 11:28:56 -0300

    aula 1

---
+++ model/DAO/filme.js
@@ -0,0 +1,81 @@
+/*******************************************************************************
+ * Objetivo: Arquivo responsável pelo CRUD de dados no MySQL referente ao filme
+ * Data: 01/10/2025
+ * Autor: Nathan
+ * Versão: 1.0
+ ******************************************************************************/
+/*
+    Exemplos de dependencias para conexão com o BD
+        Bancos de Dados relacionais
+            Sequelize -> Foi utilizado em muitos projetos desde o inicio do node
+            Prisma -> É uma dependencia atual que trabalha com BD (MySQL, PostgreSQL, SQL Server) (SQL ou ORM)
+            Knex -> É uma dependencia atual que trabalha com MySQL
+
+        Banco de Dados Não relacional
+            Mongoose -> É uma dependencia para o Mongo DB (Não relacional)
+*/
+
+// Import da dependencia do Prisma qu permite a execução de script SQL no BD
+const { PrismaClient } = require('@prisma/client')
+
+// Cria um novo objeto baseado na classe do PrismaClient
+const prisma = new PrismaClient()
+
+/*
+    $queryRawUnsafe() -> permite executar um script SQL de uma variavel 
+        e que retorna valores do banco (SELECT).
+
+    $executeRawUnsafe() -> permite executar um script SQL de uma variavel 
+        e que NÃO retorna dados do banco (INSERT, UPDATE, DELETE).
+
+    $queryRaw() -> permite executar um script SQL SEM estar em uma variavel 
+        e que retorna valores do banco (SELECT) 
+        e faz tratamentos de segurança contra SQL Injection.
+
+    $executeRaw() -> permite executar um script SQL SEM estar em uma variavel 
+        e que NÃO retorna dados do banco (INSERT, UPDATE, DELETE) 
+        e faz tratamentos de segurança contra SQL Injection.
+*/
+
+// Retorna uma lista de todos os filmes do banco de dados
+const getSelectAllMovies = async () => {
+    try {
+        // Script SQL
+        let sql = `select * from tbl_filme order by id desc`
+
+        // Encaminha para o BD o script SQL
+        let result = await prisma.$queryRawUnsafe(sql)
+
+        if (result.length > 0)
+            return result
+        else
+            return false
+
+    } catch (error) {
+        return false
+    }
+}
+
+// Retorna um filme filtrando pelo id do banco de dados
+const getSelectByIdMovies = async (id) => {
+
+}
+
+// Insere um filme novo no banco de dados
+const setInsertMovies = async () => {
+
+}
+
+// Altera um filme no banco de dados
+const setUpdateMovies = async (id) => {
+
+}
+
+// Exclui um filme pelo id no banco de dados
+const setDeleteMovies = async (id) => {
+
+}
+
+module.exports = {
+    getSelectAllMovies
+}
